3c08fc1875dd2e9725b16db78f4e6fc2fbe8370e
CauãCursino <cmfcursino@gmail.com>
2026-09-10 18:45:48 -0300

    refact(#5-7): Adição dos links para configurações e perfil

--- web/app/components/Sidebar.tsx
+++ web/app/components/Sidebar.tsx
@@ -11,11 +11,21 @@ function Sidebar() {
         { icone: "Loader", label: "Competências", rota: "/competencias" }
     ]
 
+    const Configuracoes = [
+        { icone: "Settings", label: "Configurações", rota: "/configuracoes" },
+        { icone: "CircleUserRound", label: "Meu Perfil", rota: "/perfil" },
+    ]
+
     const icones = {
         Home,
         MessagesSquare,
         FolderOpen,
         Loader
+    }
+
+    const iconesConfig = {
+        Settings,
+        CircleUserRound
     }
 
     return (
@@ -46,8 +56,21 @@ function Sidebar() {
             <div className="w-full h-1/2" />
 
             <div className="w-full h-1/10 flex flex-col justify-center items-center gap-2">
-                <Settings size={36} color={"#EF7541"} className="hover:cursor-pointer" />
-                <CircleUserRound size={36} color={"#EF7541"} className="hover:cursor-pointer" />
+                {Configuracoes.map((paginaConfig) => {
+                    const IconeConfig = iconesConfig[paginaConfig.icone as keyof typeof iconesConfig]
+
+                    return (
+                        <Link key={paginaConfig.rota} href={paginaConfig.rota} className="relative group w-12 h-20">
+                            <div className="bg-[#212838] w-full h-full flex items-center justify-center rounded-lg transition duration-300 hover:bg-[#010812] hover:cursor-pointer">
+                                <IconeConfig size={28} color={"#EF7541"} />
+                            </div>
+
+                            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-2 whitespace-nowrap bg-[#212838] text-white text-sm px-2 py-1 rounded-md opacity-0 scale-95 pointer-events-none transition-all duration-200 group-hover:opacity-100 group-hover:scale-100 font-semibold select-none">
+                                {paginaConfig.label}
+                            </span>
+                        </Link>
+                    )
+                })}
             </div>
 
         </div>
