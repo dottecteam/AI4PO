f158dd04e158ba721880d4d988064991a485e6ca
CauãCursino <cmfcursino@gmail.com>
2026-09-10 17:47:58 -0300

    feat(#5-7): Adição da Sidebar

---
+++ web/app/components/Sidebar.tsx
@@ -0,0 +1,56 @@
+import { Home, MessagesSquare, Loader, FolderOpen, CircleUserRound, Settings } from "lucide-react"
+import Logo from "./Logo"
+
+function Sidebar() {
+
+    const Paginas = [
+        { icone: "Home", label: "Página Inicial", rota: "/home" },
+        { icone: "MessagesSquare", label: "Chat", rota: "/chatbot" },
+        { icone: "FolderOpen", label: "Projetos", rota: "/projetos" },
+        { icone: "Loader", label: "Competências", rota: "/competencias" }
+    ]
+
+    const icones = {
+        Home,
+        MessagesSquare,
+        FolderOpen,
+        Loader
+    }
+
+    return (
+        <div className="bg-[#0C1322] h-full w-1/20 flex flex-col items-center justify-center gap-6">
+
+            <Logo altura={60} largura={60} />
+
+            <div className="w-full h-1/4 flex flex-col items-center justify-center gap-4 p-2">
+                {Paginas.map((pagina) => {
+                    const Icone = icones[pagina.icone as keyof typeof icones]
+
+                    return (
+                        <div key={pagina.rota} className="relative group w-12 h-20">
+                            <div className="bg-[#212838] w-full h-full flex items-center justify-center rounded-lg transition duration-300 hover:bg-[#010812] hover:cursor-pointer">
+                                <Icone size={28} color={"#EF7541"} />
+                            </div>
+
+                            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-2 whitespace-nowrap bg-[#212838] text-white text-sm px-2 py-1 rounded-md opacity-0 scale-95 pointer-events-none transition-all duration-200 group-hover:opacity-100 group-hover:scale-100">
+                                {pagina.label}
+                            </span>
+                        </div>
+                    )
+                })}
+            </div>
+
+            
+            {/* Parte do historico de conversas */}
+            <div className="w-full h-1/2" />
+
+            <div className="w-full h-1/10 flex flex-col justify-center items-center gap-2">
+                <Settings size={36} color={"#EF7541"} className="hover:cursor-pointer" />
+                <CircleUserRound size={36} color={"#EF7541"} className="hover:cursor-pointer" />
+            </div>
+
+        </div>
+    )
+}
+
+export default Sidebar
