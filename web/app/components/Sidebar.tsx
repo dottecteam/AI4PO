d7a09c762f643ecb135d42cab1ca8be3ac5edaaf
CauãCursino <cmfcursino@gmail.com>
2026-09-10 18:34:15 -0300

    feat(#5-7): Adição da navegação e layout da sidebar

--- web/app/components/Sidebar.tsx
+++ web/app/components/Sidebar.tsx
@@ -1,10 +1,11 @@
 import { Home, MessagesSquare, Loader, FolderOpen, CircleUserRound, Settings } from "lucide-react"
+import Link from "next/link"
 import Logo from "./Logo"
 
 function Sidebar() {
 
     const Paginas = [
-        { icone: "Home", label: "Página Inicial", rota: "/home" },
+        { icone: "Home", label: "Página Inicial", rota: "/" },
         { icone: "MessagesSquare", label: "Chat", rota: "/chatbot" },
         { icone: "FolderOpen", label: "Projetos", rota: "/projetos" },
         { icone: "Loader", label: "Competências", rota: "/competencias" }
@@ -18,7 +19,7 @@ function Sidebar() {
     }
 
     return (
-        <div className="bg-[#0C1322] h-full w-1/20 flex flex-col items-center justify-center gap-6">
+        <div className="bg-[#0C1322] h-full w-20 flex flex-col items-center justify-center gap-6">
 
             <Logo altura={60} largura={60} />
 
@@ -27,15 +28,15 @@ function Sidebar() {
                     const Icone = icones[pagina.icone as keyof typeof icones]
 
                     return (
-                        <div key={pagina.rota} className="relative group w-12 h-20">
+                        <Link key={pagina.rota} href={pagina.rota} className="relative group w-12 h-20">
                             <div className="bg-[#212838] w-full h-full flex items-center justify-center rounded-lg transition duration-300 hover:bg-[#010812] hover:cursor-pointer">
                                 <Icone size={28} color={"#EF7541"} />
                             </div>
 
-                            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-2 whitespace-nowrap bg-[#212838] text-white text-sm px-2 py-1 rounded-md opacity-0 scale-95 pointer-events-none transition-all duration-200 group-hover:opacity-100 group-hover:scale-100">
+                            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-2 whitespace-nowrap bg-[#212838] text-white text-sm px-2 py-1 rounded-md opacity-0 scale-95 pointer-events-none transition-all duration-200 group-hover:opacity-100 group-hover:scale-100 font-semibold select-none">
                                 {pagina.label}
                             </span>
-                        </div>
+                        </Link>
                     )
                 })}
             </div>
