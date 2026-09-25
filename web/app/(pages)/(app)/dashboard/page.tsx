9ed20ec03845e46385881a2f7dc014e00b601bf4
João Paulo <jopaulo.as8@gmail.com>
2026-09-25 11:47:34 -0300

    chore: Adicionando título dinâmico na página do dashboard

--- web/app/(pages)/(app)/dashboard/page.tsx
+++ web/app/(pages)/(app)/dashboard/page.tsx
@@ -1,10 +1,15 @@
-function Dashboard() {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Visão geral da sua área de trabalho na AI4PO.",
+};
+
+export default function Dashboard() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold text-white">Dashboard</h1>
       <p className="text-lg text-gray-400">Bem-vindo ao painel de controle!</p>
     </div>
-  )
+  );
 }
-
-export default Dashboard
